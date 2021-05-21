b176c88a40ab7aff86c410fbd0b7573c49f34c35
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-05-21 10:39:17 +0600

    improted modules

--- src/screens/PostSalesScreen.js
+++ src/screens/PostSalesScreen.js
@@ -1,24 +1,65 @@
-import React, { useContext } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { AuthContext } from '../Providers/AuthProvider'
 import { FAB } from 'react-native-paper'
 import colors from '../../assets/data/colors'
+import TextInputTaker from '../components/inputs/TextInputTaker'
+import { ScrollView } from 'react-native'
+import CurvedButton from '../components/buttons/CurvedButton'
+import { Button } from 'react-native-elements'
+import { Entypo } from '@expo/vector-icons'
+import { BUTTON_RADIUS, HEIGHT_BUTTON } from '../constants/Height_Width'
 
 const PostSalesScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, backgroundColor: 'blue' }}>
-        <Text>NANI</Text>
-      </View>
-      <View style={{ flex: 1.5, backgroundColor: 'yellow' }}></View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={{ width: '90%' }}>
+        <Text style={styles.headerText}>Location</Text>
+        <TextInputTaker />
+        <Text style={styles.headerText}>Or</Text>
 
-      <View style={{ flex: 1, backgroundColor: 'green' }}></View>
-    </View>
+        <TouchableOpacity style={styles.userBtn}>
+          <Entypo name='location-pin' size={24} color={colors.primary} />
+          <Text style={styles.userBtnTxt}>Use Current Location</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ width: '90%', backgroundColor: 'yellow' }}>
+        <Text style={styles.headerText}>Date</Text>
+
+        <TouchableOpacity style={styles.userBtn}>
+          <Entypo name='plus' size={24} color={colors.primary} />
+          <Text style={styles.userBtnTxt}>Add Date</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ backgroundColor: 'green', width: '90%' }}></View>
+    </ScrollView>
   )
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+    color: colors.darkGray,
+  },
+  userBtn: {
+    flexDirection: 'row',
+    borderColor: colors.primary,
+    borderWidth: 2,
+    borderRadius: BUTTON_RADIUS,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginHorizontal: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userBtnTxt: {
+    color: colors.primary,
   },
 })
 export default PostSalesScreen
