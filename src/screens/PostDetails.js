c8a3b458f88914b9f13d3e344c046c7e7ad4ba86
hossainzarif <zarifh493@gmail.com>
2021-06-07 13:45:30 +0600

    adress showing

--- src/screens/PostDetails.js
+++ src/screens/PostDetails.js
@@ -1,13 +1,43 @@
 import React from "react"
-import { Text } from "react-native"
-import { View } from "react-native"
+import { View, ScrollView, Text, SafeAreaView, StyleSheet } from "react-native"
+import colors from "../../assets/data/colors"
+import { Card, ListItem, Button, Icon } from "react-native-elements"
 
 const PostDetails = (props) => {
+  const { address } = props
   return (
-    <View>
-      <Text>THIS IS POST DETAILS</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+        <View style={{ width: "90%", marginTop: 10, marginBottom: 10 }}>
+          <Text style={styles.headerText}>Location *</Text>
+          <Card
+            containerStyle={{
+              borderRadius: 10,
+              elevation: 5,
+            }}
+          >
+            <Text style={styles.locationText}>{address}</Text>
+          </Card>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.darkGray,
+  },
+
+  userBtnTxt: {
+    color: colors.primary,
+  },
+  locationText: { textAlign: "justify", fontSize: 15 },
+})
 
 export default PostDetails
