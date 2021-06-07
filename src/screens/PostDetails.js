6884c6f3a7ecc09ad4ccc3da7ae4731e9889b6ec
hossainzarif <zarifh493@gmail.com>
2021-06-07 16:50:09 +0600

    new package

--- src/screens/PostDetails.js
+++ src/screens/PostDetails.js
@@ -10,8 +10,16 @@ import {
 import colors from "../../assets/data/colors"
 import { Card, ListItem, Button, Icon } from "react-native-elements"
 import DateCard from "../cards/DateCard"
+import { SliderBox } from "react-native-image-slider-box"
+import { Colors } from "react-native/Libraries/NewAppScreen"
+
 const PostDetails = ({ route }) => {
-  const { address, dates, details } = route.params
+  const { address, dates, details, images } = route.params
+  const imagesArr = [
+    "https://s-media-cache-ak0.pinimg.com/originals/ee/51/39/ee5139157407967591081ee04723259a.png",
+    "https://s-media-cache-ak0.pinimg.com/originals/40/4f/83/404f83e93175630e77bc29b3fe727cbe.jpg",
+    "https://s-media-cache-ak0.pinimg.com/originals/8d/1a/da/8d1adab145a2d606c85e339873b9bb0e.jpg",
+  ]
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{ alignItems: "center" }}>
@@ -26,7 +34,7 @@ const PostDetails = ({ route }) => {
 
           <FlatList
             data={dates}
-            renderItem={({ item, index }) => (
+            renderItem={({ item }) => (
               <DateCard time={item} showbutton={false} />
             )}
             horizontal
@@ -36,6 +44,36 @@ const PostDetails = ({ route }) => {
 
         <View style={styles.sections}>
           <Text style={styles.headerText}>Photos</Text>
+
+          {images ? (
+            <View style={{ width: "100%", backgroundColor: "white" }}>
+              <SliderBox
+                images={images}
+                dotColor={colors.primary}
+                paginationBoxVerticalPadding={20}
+                circleLoop
+                resizeMethod={"resize"}
+                resizeMode={"contain"}
+                paginationBoxStyle={{
+                  alignItems: "center",
+                  alignSelf: "center",
+                  justifyContent: "center",
+                }}
+                ImageComponentStyle={{
+                  borderRadius: 15,
+                  backgroundColor: "white",
+                }}
+                imageLoadingColor={colors.primary}
+              />
+            </View>
+          ) : (
+            <Card containerStyle={styles.cardStyle}>
+              <Card.Image
+                source={require("../../assets/unav.png")}
+                style={{ resizeMode: "contain" }}
+              ></Card.Image>
+            </Card>
+          )}
         </View>
         <View style={styles.sections}>
           <Text style={styles.headerText}>Details</Text>
