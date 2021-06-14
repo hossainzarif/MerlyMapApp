09613cc7d7d52d0d1e62286ee8b19ecc94609f35
hossainzarif <zarifh493@gmail.com>
2021-06-14 13:09:23 +0600

    colors changed

--- assets/data/colors.js
+++ assets/data/colors.js
@@ -1,12 +1,12 @@
 const colors = {
-  gray: '#D1D3D2',
-  darkGray: '#676767',
-  black: '#0C0D0E',
-  white: '#FBFCFE',
-  primary: '#5dad59',
-  primary_dark: '#268621',
-  primary_fade: '#deeedd',
-  warning: '#C3272B',
+  gray: "#E3E3E3",
+  darkGray: "#676767",
+  black: "#0C0D0E",
+  white: "#FBFCFE",
+  primary: "#5dad59",
+  primary_dark: "#268621",
+  primary_fade: "#deeedd",
+  warning: "#C3272B",
 }
 
 export default colors
