680e3772f57c048fa6df6e03db308500b46c1db9
hossainzarif <zarifh493@gmail.com>
2021-06-15 00:30:24 +0600

    chatscreen slighly implemented

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,29 +1,70 @@
 import { View, Text } from "react-native"
-import React, { useState, useCallback, useEffect } from "react"
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useContext,
+  useLayoutEffect,
+} from "react"
 import { GiftedChat, Bubble } from "react-native-gifted-chat"
 import colors from "../../assets/data/colors"
+import { AuthContext } from "../Providers/AuthProvider"
+import { db } from "../utils/firebase"
+import * as firebase from "firebase"
+import Loading from "../custom/Loading"
 
-const ChatScreen = ({ route }) => {
+const ChatScreen = () => {
   const [messages, setMessages] = useState([])
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: require("../../assets/av_ed.png"),
-        },
-      },
-    ])
-  }, [])
+  const { user } = useContext(AuthContext)
+  const [chatLoading, setchatLoading] = useState(false)
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: "Hello developer",
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: "React Native",
+  //         avatar: require("../../assets/av_ed.png"),
+  //       },
+  //     },
+  //   ])
+  // }, [])
   const onSend = useCallback((messages = []) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     )
+
+    const { _id, createdAt, text, user } = messages[0]
+    firebase.firestore().collection("chats").add({
+      _id,
+      createdAt,
+      text,
+      user,
+    })
   }, [])
+
+  useLayoutEffect(() => {
+    setchatLoading(true)
+    const unsubscribe = db
+      .collection("chats")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user,
+          }))
+        )
+      )
+
+    return unsubscribe
+  }, [])
+
   function renderBubble(props) {
     return (
       <Bubble
@@ -39,16 +80,22 @@ const ChatScreen = ({ route }) => {
       />
     )
   }
-  return (
-    <GiftedChat
-      renderBubble={renderBubble}
-      messages={messages}
-      onSend={(messages) => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-    />
-  )
+
+  if (chatLoading) {
+    return <Loading />
+  } else {
+    return (
+      <GiftedChat
+        renderBubble={renderBubble}
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: user.uid,
+          name: user.displayName,
+        }}
+      />
+    )
+  }
 }
 
 export default ChatScreen
