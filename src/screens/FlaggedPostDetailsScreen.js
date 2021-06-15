5b0ae4555319bf60fd424b38760f57a10f1fa6c7
hossainzarif <zarifh493@gmail.com>
2021-06-15 23:12:15 +0600

    warning sign added

--- src/screens/FlaggedPostDetailsScreen.js
+++ src/screens/FlaggedPostDetailsScreen.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react"
 import { StyleSheet } from "react-native"
 import { View, Text } from "react-native"
+import Loading from "../custom/Loading"
 import { db } from "../utils/firebase"
-
+import { AntDesign } from "@expo/vector-icons"
+import colors from "../../assets/data/colors"
 const FlaggedPostDetailsScreen = ({ route }) => {
   const { name, user_id, email } = route.params
   const [loading, setloading] = useState(false)
+  const [flaggers, setflaggers] = useState(0)
 
   const loadPosts = async () => {
     try {
@@ -15,14 +18,9 @@ const FlaggedPostDetailsScreen = ({ route }) => {
         .doc(user_id)
         .collection("flaggers")
         .onSnapshot((querySnapshot) => {
-          let temp_posts = []
-          querySnapshot.forEach((doc) => {
-            temp_posts.push({
-              id: doc.id,
-              data: doc.data(),
-            })
-          })
+          setflaggers(querySnapshot.size)
           setloading(false)
+          console.log(flaggers)
         })
     } catch (error) {
       Alert.alert("Error:", error.message)
@@ -35,18 +33,27 @@ const FlaggedPostDetailsScreen = ({ route }) => {
     loadPosts()
   }, [])
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.textdata}>User Name : {name}</Text>
-      <Text style={styles.textdata}>User Id : {user_id}</Text>
-      <Text style={styles.textdata}>User Email : {email}</Text>
-    </View>
-  )
+  if (loading) {
+    return <Loading />
+  } else {
+    return (
+      <View style={styles.container}>
+        {flaggers >= 3 ? (
+          <AntDesign name='warning' size={60} color={colors.warning} />
+        ) : null}
+        <Text style={styles.textdata}>User Name : {name}</Text>
+        <Text style={styles.textdata}>User Id : {user_id}</Text>
+        <Text style={styles.textdata}>User Email : {email}</Text>
+        <Text style={styles.textdata}>Flagged By : {flaggers} Users</Text>
+      </View>
+    )
+  }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   textdata: {
     fontSize: 18,
