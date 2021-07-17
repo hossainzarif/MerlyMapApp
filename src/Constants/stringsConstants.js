e2723d725cde16e315a999602f4f1f19896e143d
hossainzarif <zarifh493@gmail.com>
2021-07-18 00:08:56 +0600

    new string added

--- src/Constants/stringsConstants.js
+++ src/Constants/stringsConstants.js
@@ -4,7 +4,8 @@ export const TERMS =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's "
 
 export const ONBOARDING_1 =
-  "Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper"
+  'Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper'
 
 export const ONBOARDING_2 =
-  "Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper"
+  'Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper Done with React Native Onboarding Swiper'
+export const ADMIN_MAIL = 'garagesalesmap1@gmail.com'
